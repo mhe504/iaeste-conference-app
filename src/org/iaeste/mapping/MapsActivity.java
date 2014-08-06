100865302aaadd2875e29d87bdfcbb024e546b78
mhe504 <mhe504@york.ac.uk>
2014-08-06 08:02:31 +0100

    Fixed Map

--- src/org/iaeste/mapping/MapsActivity.java
+++ src/org/iaeste/mapping/MapsActivity.java
@@ -10,134 +10,115 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.LinkedList;
-import java.util.List;
 
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.iaeste.R;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.util.constants.MapViewConstants;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Environment;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.iaeste.R;
+public class MapsActivity extends Activity implements MapViewConstants {
+   
+    MyItemizedOverlay myItemizedOverlay = null;
+    MyItemizedOverlay myItemizedOverlay2 = null;
+	private GeoPoint currentPosition;
+	private GeoPoint confLocation = new GeoPoint(50.780194, 15.173639);
+	MapView mapView;
+	private IMapController controller;
 
-public class MapsActivity extends Activity {
-	protected static final String PROVIDER_NAME = LocationManager.GPS_PROVIDER;
-    private MapController osmViewController;
-    private MapView osmView;
-    private double latitude = 50.780194;
-    private double longitude = 15.173639;
-    private GeoPoint currentPosition;
-    private LocationMarker myLocationOverlay;
-    
-    private List<PoiMarker> markers;
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        
-        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
-        	copyTilesToSd();
-        }
-        else{
-            displayLongToast("Can not load cached map. SD Card could not be accessed.");
-        }
-  
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps);
-
-        osmView = (MapView) findViewById(R.id.mapview);
-        osmViewController = osmView.getController();
-
-        osmView.setTileSource(TileSourceFactory.MAPNIK);
-        osmView.setBuiltInZoomControls(true);
-        osmViewController.setZoom(15);
-
-        currentPosition = new GeoPoint(latitude,longitude);
-        osmViewController.setCenter(currentPosition);
+        copyTilesToSd();
+        mapView = (MapView) findViewById(R.id.mapview);
+        controller = mapView.getController();
+        mapView.setBuiltInZoomControls(true);
         
-        myLocationOverlay = new LocationMarker(this);
+        Drawable marker=getResources().getDrawable(R.drawable.marker);
+        Drawable locationMarker=getResources().getDrawable(android.R.drawable.ic_menu_mylocation);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+        locationMarker.setBounds(0, markerHeight, markerWidth, 0);
+         
+        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+         
+        myItemizedOverlay = new MyItemizedOverlay(marker, resourceProxy);
+        myItemizedOverlay2 = new MyItemizedOverlay(locationMarker, resourceProxy);
+        mapView.getOverlays().add(myItemizedOverlay);
+        mapView.getOverlays().add(myItemizedOverlay2);
+        new Handler().postDelayed(new Thread () {
+            @Override
+            public void run() {
+            	controller.animateTo(confLocation);
+            }
+        }, 600);
+        controller.setZoom(14);
+        //controller.animateTo(myPoint1);
+        mapView.invalidate();
         
-        populateMarkers();
-        
-        setupSpinner();
-        
+        myItemizedOverlay.addItem(confLocation, "Conference Location", "Conference Location");
         setupGPS();
-        
-        Button centreButton = (Button)findViewById(R.id.centreButton);
-        centreButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-	            osmViewController.setCenter(currentPosition);
-			}
-		});
-        
-        Button hideButton = (Button)findViewById(R.id.hideButton);
-        hideButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				
-				Button sender = (Button)v;
-				if ("Hide Labels".equals(sender.getText()))
-				{
-					myLocationOverlay.setLabelVisibility(false);
-					for (PoiMarker marker: markers)
-					{
-						marker.setLabelVisibility(false);
-					}	
-					sender.setText("Show Labels");
-				}
-				else
-				{
-					myLocationOverlay.setLabelVisibility(true);
-					for (PoiMarker marker: markers)
-					{
-						marker.setLabelVisibility(true);
-					}	
-					sender.setText("Hide Labels");
-				}
-				osmView.invalidate();
-			}
-		});
-        
-    	Button dirButton = (Button) findViewById(R.id.directionsButton);
-    	dirButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View arg0) {
+         
+    } 
 
-			}
-		});
+    
+	
+	private void setupGPS() {
+		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         
-    }
-
-	private void displayLongToast(String message) {
-		final Toast errorMessage = Toast.makeText(getBaseContext(), 
-												  message,
-												  Toast.LENGTH_SHORT);
-
-		new CountDownTimer(6000, 1000)
-		{
-		    public void onTick(long millisUntilFinished) {errorMessage.show();}
-		    public void onFinish() {}
-		}.start();
+		setLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, new LocationListener() {
+			
+        	public void onLocationChanged(Location arg0) {
+        		setLocation(arg0);
+        	}
+        	
+        	//unimplemented methods
+        	public void onProviderDisabled(String arg0) {}
+        	public void onProviderEnabled(String arg0) {}
+        	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
+		});
 	}
-
+	
+	private void setLocation(Location location) {
+		
+		if(location != null)
+		{
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+			
+			currentPosition = new GeoPoint(latitude, longitude);     
+			
+			TextView locationTextView = (TextView) findViewById(R.id.locationTextView);
+			locationTextView.setText("Location Found");
+			locationTextView.setWidth(90);
+			myItemizedOverlay.clear();
+			myItemizedOverlay2.clear();
+			myItemizedOverlay.addItem(confLocation, "Conference Location", "myPoint1");
+	        myItemizedOverlay2.addItem(currentPosition, "Your Location", "myPoint2");
+	        
+	        controller.setCenter(currentPosition);
+	        mapView.invalidate();
+			
+		}
+	}
+	
 	private void copyTilesToSd() {
 
 		try {
@@ -171,104 +152,18 @@ public class MapsActivity extends Activity {
 			displayLongToast("Can not load cached map. IOException.");
 		}
 	}
-
-	private void setupGPS() {
-		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        
-		setLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, new LocationListener() {
-			
-        	public void onLocationChanged(Location arg0) {
-        		setLocation(arg0);
-        	}
-        	
-        	//unimplemented methods
-        	public void onProviderDisabled(String arg0) {}
-        	public void onProviderEnabled(String arg0) {}
-        	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
-		});
-	}
 	
-	private void setLocation(Location location) {
-		
-		if(location != null)
+	private void displayLongToast(String message) {
+		final Toast errorMessage = Toast.makeText(getBaseContext(), 
+												  message,
+												  Toast.LENGTH_SHORT);
+
+		new CountDownTimer(6000, 1000)
 		{
-			latitude = location.getLatitude();
-			longitude = location.getLongitude();
-			
-            currentPosition = new GeoPoint(latitude, longitude);     
-            
-            myLocationOverlay.setmLocation(currentPosition);
-            osmView.getOverlays().add(myLocationOverlay);
-            
-            TextView locationTextView = (TextView) findViewById(R.id.locationTextView);
-            locationTextView.setText("Location Found");
-            locationTextView.setWidth(90);
-            
-            Button centreButton = (Button)findViewById(R.id.centreButton);
-            centreButton.setVisibility(Button.VISIBLE);
-           
-		}
+		    public void onTick(long millisUntilFinished) {errorMessage.show();}
+		    public void onFinish() {}
+		}.start();
 	}
 
-	private void setupSpinner() {
-		Spinner spinner = (Spinner) findViewById(R.id.markerSpinner);
-        ArrayAdapter <CharSequence> adapter = new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
-        for (PoiMarker marker : markers)
-        {
-        	adapter.add(marker.getLabel());
-        }
-        adapter.add("ALL");
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-		        
-				for (PoiMarker marker : markers)
-		        {
-		        	if (marker.getLabel().equals(arg0.getSelectedItem().toString()))
-		        	{
-		                osmView.getOverlays().add(marker);
-			            osmViewController.setCenter(marker.getMyLocation());
-		        	}
-		        	else if (arg0.getSelectedItem().toString().equals("ALL"))
-		                osmView.getOverlays().add(marker);
-		        	else
-		        	{
-		        		osmView.getOverlays().remove(marker);
-		        	}
-		        	
-		        	Button dirButton = (Button) findViewById(R.id.directionsButton);
-		        	Button hideButton = (Button) findViewById(R.id.hideButton);
-		        	if (arg0.getSelectedItem().toString().equals("Paddy's  Palace "))
-		        	{
-		        		dirButton.setVisibility(Button.VISIBLE);
-		        		hideButton.setVisibility(Button.GONE);
-		        	}
-		        	else
-		        	{
-		        		dirButton.setVisibility(Button.GONE);
-		        		hideButton.setVisibility(Button.VISIBLE);	
-		        	}
-		        }
-				osmView.invalidate();
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {}
-		});
-	}
-
-    private void populateMarkers()
-    {
-    	markers = new LinkedList<PoiMarker>();
-    	
-    	PoiMarker item = new PoiMarker(this, "Hostel Artur-Janov nad Nisou ", new GeoPoint(50.780194, 15.173639));
-    	markers.add(item);
-   	
-    	
-    }
     
 }
